ae232f9684d30351dc9950fd9bbc44df22b85837
IsaacH <Isaac.hathaway28@gmail.com>
2022-03-24 12:39:34 -0600

    edit Profile layout

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 import * as profileService from '../../services/profileService'
 
 const Profiles = () => {
@@ -11,16 +12,25 @@ const Profiles = () => {
 
   return (
     <>
-      <h1>Hello. This is a list of all the profiles.</h1>
-      {profiles.length ? 
+      <div className="card" activeStyle={{width: 18}}>
+          {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
+        {profiles.length ? 
         <>
           {profiles.map(profile=>
-            <p key={profile._id}>{profile.name}</p>
+            <>
+              <p key={profile._id}>{profile.name}</p>
+              <div className="card-body">
+                <h5 cclassName="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <NavLink to="/" className="btn btn-primary">Go somewhere</NavLink>
+              </div>
+            </>
           )}
         </>
       :
         <p>No profiles yet</p>
       }
+      </div>
     </>
   )
 }
