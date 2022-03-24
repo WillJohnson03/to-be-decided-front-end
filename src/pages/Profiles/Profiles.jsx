432e1288ee7ffce385ab4c3c210f200bf5c0906d
will <wbrycejohnson031086@gmail.com>
2022-03-24 14:47:19 -0400

    resolve profile conflict

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import * as profileService from '../../services/profileService'
-import { NavLink } from 'react-router-dom'
 
 const Profiles = () => {
   const [profiles, setProfiles] = useState([])
