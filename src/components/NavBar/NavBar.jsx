fab192094edbccf644d1f92b190e55dde4f2701b
IsaacH <Isaac.hathaway28@gmail.com>
2022-03-26 15:20:28 -0600

    fix navbar and remove unnecessary css from app.css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,43 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">TBD</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo02"></div>
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li className='nav-link'>Welcome, {user.name}</li>
-            <li className="nav-item"><Link className='nav-link' to="/allmedia">All Media</Link></li>
-            <li className="nav-item"><Link className='nav-link' to="/squads">Squads</Link></li>
-            <li className="nav-item"><Link className='nav-link' to="/profiles">Profiles</Link></li>
-            <li className="nav-item"><Link className='nav-link' to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li className="nav-item"><Link className='nav-link' to="/changePassword">Change Password</Link></li>
-          </ul>
+          <NavLink className="navbar-brand" to="/">TBD</NavLink>
+            <button 
+              className="navbar-toggler" 
+              type="button" 
+              data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" 
+              aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active nav-link">Welcome, {user.name}</li>
+              <li className='nav-item'>
+                <NavLink className="nav-link" to='/allmedia'>All Media <span className='sr-only'>(current)</span></NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className='nav-link' to="/squads">Squads</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className='nav-link' to="/profiles">Profiles</NavLink></li>
+              <li className="nav-item">
+                <NavLink className='nav-link' to="" onClick={handleLogout}>LOG OUT</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className='nav-link' to="/changePassword">Change Password</NavLink>
+              </li>
+            </ul>
+          </div>
         </nav>
       :
         <nav>
           <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+            <li><NavLink to="/login">Log In</NavLink></li>
+            <li><NavLink to="/signup">Sign Up</NavLink></li>
           </ul>
         </nav>
       }
