efcacf98a58de382a59471d5d55eb6caae134ab4
IsaacH <Isaac.hathaway28@gmail.com>
2022-03-26 16:36:33 -0600

    add bootstrap cards to profile page and make profiles cards mobile responsive

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -12,28 +12,29 @@ const Profiles = () => {
   }, [])
 
   return (
-    <>
-      <div className='card-container'>
-        <div className="card">
-            {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
-          {profiles.length ?  
+    <div>
+      {profiles.length ?  
           <>
+            <div className='card-container'>
             {profiles.map(profile=>
-                <div key={profile._id} className="card-body">
-                  <h5 className="card-title" >{profile.name}</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <NavLink to="/" className="btn btn-primary">View profile</NavLink>
+                <div key={profile._id} className="card">
+                  <img className="card-img-top" src="https://picsum.photos/286/180" alt="https://picsum.photos/179/200" />
+                  <div className='card-body'>
+                    <h5 className="card-title" >{profile.name}</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <NavLink to="/" className="btn btn-secondary">View profile</NavLink>
+                  </div>
                 </div>
-              
             )}
+            </div>
           </>
         :
           <p>No profiles yet</p>
         }
-        </div>
-      </div>
-    </>
+    </div>
   )
 }
- 
+
 export default Profiles
