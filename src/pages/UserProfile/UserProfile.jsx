31a47be2d2e715cca1a9d09e644ec530879aed48
Wolf <d.wolftripp@gmail.com>
2022-03-28 08:24:27 -0700

    add path for userProfiles

---
+++ src/pages/UserProfile/UserProfile.jsx
@@ -0,0 +1,18 @@
+
+
+
+const UserProfile = () => {
+  <div>
+    <p>Squads</p>
+    <form action="/">
+      <p>
+        Your Favorites
+        {/* {user.id.equals(user?.profile._id)} */}
+        <button id='edit-btn' className='btn-md'>edit</button>
+      </p>
+      <p>Favorite movie:</p>
+      <p>Favorite Videogame:</p>
+      <p>Favorite Boardgame:</p>
+    </form>
+  </div>
+}
