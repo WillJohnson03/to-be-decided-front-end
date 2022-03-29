08b6f0d5ed4e6339a5ad3e754dba1d72914cb075
will <wbrycejohnson031086@gmail.com>
2022-03-29 19:36:40 -0400

    add picsum photo if no squad photo

--- src/pages/SquadDetails/SquadDetails.jsx
+++ src/pages/SquadDetails/SquadDetails.jsx
@@ -4,7 +4,11 @@ const SquadDetails = (props) => {
   return ( 
     <div className='card-container'>
       <div className='card'>
+        {props.squad.avatar ?
         <img className="card-img-top" src={props.squad.avatar} alt={props.squad.avatar} />
+        :
+        <img className="card-img-top" src="https://picsum.photos/286/180" alt="https://picsum.photos/179/200" />
+        }
         <div className='card-body'>
           <h5 className="card-title" >{props.squad.name}</h5>
           <p className="card-text">
