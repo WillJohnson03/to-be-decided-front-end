ac8c24d84cbe3edc798af242e2a278f2d0ed798d
IsaacH <Isaac.hathaway28@gmail.com>
2022-03-29 17:31:54 -0600

    create add movie function and fix errors in routing

--- src/pages/VideoGameDetails/VideoGameDetails.jsx
+++ src/pages/VideoGameDetails/VideoGameDetails.jsx
@@ -1,19 +1,23 @@
+import './VideoGameDetails.css'
 import { useParams, useRef, useState, useLocation } from "react-router-dom";
 
-const VideoGameDetails = () => {
+const VideoGameDetails = (handleRemoveVideoGame, handleAddVideoGame) => {
   const location = useLocation()
   const videoGame = location.state.result
   console.log(videoGame)
   return ( 
 
-    <>
-      <div className='videogame'>
-        <img src={videoGame.background_image} alt="videoGame" />
-        <h3>{videoGame.name}</h3>
-        <p>{videoGame.description}</p>
+    <div className="card-container">
+      <div className='videogame card'>
+        <img className='card-img-top' src={videoGame.background_image} alt="videoGame" />
+        <div className='card-body'>
+          <h3 className='card-title'>{videoGame.slug}</h3>
+          <button className='add-game' onClick={handleAddVideoGame}>Add VideoGame</button>
+        </div>
       </div>
-    </>
-   );
+    </div>
+  );
 }
+
 
 export default VideoGameDetails;
--- src/services/movies-api-calls.js
+++ src/services/movies-api-calls.js
@@ -1,10 +1,5 @@
 const BASE_URL = process.env.REACT_APP_BACKEND_SERVER_URL
 
-// export function getMoviesList() {
-//   return fetch(`${BASE_URL}/api/movies`)
-//   .then(res => res.json())
-// }
-
 export function searchMovie(name) {
   // console.log(name)
   return fetch(`${BASE_URL}/api/movies/${name}`)
