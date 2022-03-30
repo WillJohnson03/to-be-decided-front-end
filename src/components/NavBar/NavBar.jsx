0a041898bd57ee72cf310356777fa87d38f67a9d
will <wbrycejohnson031086@gmail.com>
2022-03-30 11:50:16 -0400

    make navbar styling uniform whether logged in or out

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -19,9 +19,6 @@ const NavBar = ({ user, handleLogout }) => {
               <li className='nav-item'>
                 <NavLink className="nav-link" to='/allmedia'>All Media <span className='sr-only'>(current)</span></NavLink>
               </li>
-              {/* <li className="nav-item">
-                <NavLink className='nav-link' to="/boardgames">Board Games</NavLink>
-              </li> */}
               <li className="nav-item">
                 <NavLink className='nav-link' to="/squads">Squads</NavLink>
               </li>
@@ -37,11 +34,22 @@ const NavBar = ({ user, handleLogout }) => {
           </div>
         </nav>
       :
-        <nav>
-          <ul>
-            <li><NavLink to="/login">Log In</NavLink></li>
-            <li><NavLink to="/signup">Sign Up</NavLink></li>
-          </ul>
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <NavLink className="navbar-brand" to="/">TBD</NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item nav-link"><NavLink className="nav-link" to="/login">Log In</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/signup">Sign Up</NavLink></li>
+            </ul>
+          </div>
         </nav>
       }
     </>
