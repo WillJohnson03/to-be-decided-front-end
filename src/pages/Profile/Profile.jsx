19388ae079a0f4ff9b5bd22498666ea11cb5757e
IsaacH <Isaac.hathaway28@gmail.com>
2022-03-30 10:12:55 -0600

    display user movie collection to profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { getProfile } from '../../services/profileService'
 import { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Profile = (props) => {
   const [profile, setProfile] = useState([])
@@ -12,10 +13,47 @@ const Profile = (props) => {
         setProfile(profileData)
       })
   }, [])
-  
+  console.log(profile.movie);
   return (
     <>
+      <div>
       <h3>{profile.name}</h3>
+      </div>
+      <div>
+        <h3>Movie list</h3>
+          {profile.movie?.map((movies, index) =>
+              <div className='card-container' key={movies.title}>
+                <div  className='card'>
+                <img className="card-img-top" src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movies.poster_path}`} alt={movies.title}/>
+                <div className='card-body'>
+                  <h5 className="card-title">{movies.title}</h5>
+                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Details
+                  </button>
+                  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLongTitle">{movies.title}</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          {movies.overview} 
+                          <br /> 
+                          Release date: <span>{movies.release_date}</span>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+      </div>
     </>
   );
 }
