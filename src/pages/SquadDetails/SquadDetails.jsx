d2e39ad82f6a634749f69a045e34a89a978bb47e
zackbeckwith <z.beckwith99@gmail.com>
2022-03-31 16:23:36 -0400

    css updates

--- src/pages/SquadDetails/SquadDetails.jsx
+++ src/pages/SquadDetails/SquadDetails.jsx
@@ -7,14 +7,14 @@ const SquadDetails = (props) => {
         {props.squad.avatar ?
         <img className="card-img-top" src={props.squad.avatar} alt={props.squad.avatar} />
         :
-        <img className="card-img-top" src="https://picsum.photos/286/180" alt="https://picsum.photos/179/200" />
+        <img className="card-img-top" src="https://i.imgur.com/3FLcsWl.png" alt="https://i.imgur.com/3FLcsWl.png" />
         }
         <div className='card-body'>
           <h5 className="card-title" >{props.squad.name}</h5>
           <p className="card-text">
           {props.squad.creator.name}'s squad
           </p>
-          <NavLink to={`/squad/${props.squad._id}`} className="btn btn-secondary" state={props.squad}>View Squad</NavLink>
+          <NavLink to={`/squad/${props.squad._id}`} className="btn btn-outline-*" state={props.squad}>View Squad</NavLink>
         </div>
       </div>
     </div>
